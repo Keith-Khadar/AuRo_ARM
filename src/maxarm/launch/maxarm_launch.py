0e4048e512aacbd49708f882d33055ea30f8b190
Keith Khadar <keith.khadar@gmail.com>
2025-04-22 18:14:15 +0000

    maxarm launch file and perception code

---
+++ src/maxarm/launch/maxarm_launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='maxarm',
+            namespace='odom',
+            executable='arm_pos_publisher',
+            output='screen'
+        ),
+        Node(
+            package='maxarm',
+            namespace='set_pos',
+            executable='arm_set_pos',
+            output='screen'
+        ),
+    ])
