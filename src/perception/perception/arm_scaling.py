beceff8f706a130a30ed8f45aae2ad08d2a8fb78
Thomas Chang <tlchang02@gmail.com>
2025-04-28 15:45:47 -0400

    Added DFS, BFS, and got bitmap working
    All code used to test is in a main function inside of arm_scaling.py. Pathing returns bitmap and direction list for actual arm movements.

--- src/perception/perception/arm_scaling.py
+++ src/perception/perception/arm_scaling.py
@@ -2,13 +2,17 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import grid
+import pathing
 
 RES_MULT = 90
 ARUCO_SZ = 0.984252 # 25mm
 PAGE_MARGIN = 0.39
 
-if __name__ == '__main__' : 
-    
+''' Performs simply homography, mapping maze image to 8.5 inch x 11 inch sheet of paper.
+    Automatically detects bounds using ArUco markers.
+    Returns scaled image.
+'''
+def mazeHomography(image):  
     # Predefined based on 8.5 x 11 printer paper. Units in inches. Top left corners of ArUco Markers on vertical sheet.
     height = 11 * RES_MULT
     width = 8.5 * RES_MULT
@@ -21,7 +25,6 @@ if __name__ == '__main__' :
 
 
     # Obtain coordinates using ArUco marker detection
-    image = cv2.imread("data/sample2.jpg")
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
     aruco_dic = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
@@ -29,9 +32,10 @@ if __name__ == '__main__' :
     if (len(corners) > 0):
         print("ArUco Markers detected")
         image_aruco = cv2.aruco.drawDetectedMarkers(image.copy(), corners, ids)
-        cv2.imshow("ArUco Markers in Image", image_aruco)
+        #cv2.imshow("ArUco Markers in Image", image_aruco)
         for i in range(len(corners)):
-            print(corners[i][0], '\n')
+            #print(corners[i][0], '\n')
+            pass
     else:
         print("ERROR: Failed to detect markers")
         exit()
@@ -46,12 +50,30 @@ if __name__ == '__main__' :
     # Perform homography
     H, retVal = cv2.findHomography(pts_image, pts_world)
     image_scaled = cv2.warpPerspective(image, H, ((int)(width - (2*PAGE_MARGIN+ARUCO_SZ)), (int)(height - (2*PAGE_MARGIN+ARUCO_SZ))))
-    print("H matrix:\n", H)
 
-    # Output image
+    # Output image 
+    return image_scaled
+
+    
+if __name__ == '__main__' : 
+    image = cv2.imread("data/sample2.jpg")
+    result = mazeHomography(image)
     #cv2.imshow("Source", image)
-    cv2.imshow("Scaled", image_scaled)
-    grid.toGrid(image_scaled)
-    np.savetxt('bitmap.txt', grid.toBitmapGrid(image_scaled), fmt='%d')
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.imshow("Scaled", result)
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    # (Optional): See input image at bitmap resolution
+    #grid.toGrid(image_scaled)
+    
+    # Testing grid with start and end
+    image2 = cv2.imread("data/point_test.jpg")
+    image2_bitmap, image2_start, image2_end = grid.toBitmapGrid(image2)
+    np.savetxt('bitmap.txt', image2_bitmap, fmt='%d')
+    
+    image2_dfs_bitmap, image2_dfs_dir = pathing.pathing_dfs(image2_bitmap, image2_start, image2_end)
+    np.savetxt('dfs_bitmap.txt', image2_dfs_bitmap, fmt='%d')
+
+    image2_bfs_bitmap, image2_bfs_dir = pathing.pathing_bfs(image2_bitmap, image2_start, image2_end)
+    np.savetxt('bfs_bitmap.txt', image2_bfs_bitmap, fmt='%d')    
+    
---
+++ src/perception/perception/pathing.py
@@ -0,0 +1,145 @@
+import numpy as np
+from collections import deque
+
+''' Performs depth first search (DFS) algorithm using input bitmap post image processing.
+    Returns bitmap as well as directions for the arm to mimic.
+    Directions are formatted in (dy,dx) where movement is by one grid square.
+    Pathing is shown on bitmap as 4s
+''' 
+def pathing_dfs(bitmap, start, end):
+    bitmap_dfs = bitmap.copy()
+    rows, cols = bitmap.shape
+    visited = np.zeros((rows, cols), dtype = bool)
+    
+    # Map for pathing later on
+    parent = {} 
+
+    paths = {
+        (-1, 0),
+        (1,0),
+        (0,-1),
+        (0, 1)
+    }
+
+    # In the beginning only start is initialized
+    stack = [start]
+    visited[start] = True
+
+    while stack:
+        x,y = stack.pop()
+        
+        # End reached
+        if (x,y) == end:
+            break
+        
+        # Else investigate other paths
+        for (dy, dx) in paths:
+            new_y = y + dy
+            new_x = x + dx
+            # Check boundary conditions
+            if 0 <= new_y < rows and 0 <= new_x < cols:
+                if not visited[new_y, new_x] and bitmap_dfs[new_y, new_x] != 1:
+                    visited[new_y, new_x] = True
+                    parent[(new_x, new_y)] = (x, y)
+                    stack.append((new_x, new_y))
+
+    # Get the actual path
+    if end not in parent:
+        print("DFS: Not actual path found\n")
+        return bitmap_dfs, []
+    
+    path = []
+    directions = []
+    position = end
+
+    while position != start:
+        path.append(position)
+
+        prev = parent[position]
+        path_to_x = position[0] - prev[0]
+        path_to_y = position[1] - prev[1]
+        directions.append((path_to_y, path_to_x))
+        
+        position = prev
+    
+    path.append(start)
+    path.reverse()
+    directions.reverse()
+
+    # Draw path on bitmap
+    for (x,y) in path:
+        bitmap_dfs[y,x] = 4
+
+    return bitmap_dfs, directions
+
+''' Performs breadth first search (BFS) algorithm using input bitmap post image processing.
+    Returns bitmap as well as directions for the arm to mimic.
+    Directions are formatted in (dy,dx) where movement is by one grid square.
+    Pathing is shown on bitmap as 4s
+''' 
+def pathing_bfs(bitmap, start, end):
+    rows, cols = bitmap.shape
+    visited = np.zeros((rows, cols), dtype = bool)
+    bitmap_bfs = bitmap.copy()
+
+    # Map for pathing later on
+    parent = {} 
+
+    paths = {
+        (-1, 0),
+        (1,0),
+        (0,-1),
+        (0, 1)
+    }
+
+    # In the beginning only start is initialized
+    queue = deque()
+    queue.append((start))
+    visited[start] = True
+
+    while queue:
+        x,y = queue.popleft()
+
+        # End reached
+        if (x,y) == end:
+            break
+        
+        # Else investigate other paths
+        for (dy, dx) in paths:
+            new_y = y + dy
+            new_x = x + dx
+            # Check boundary conditions
+            if 0 <= new_y < rows and 0 <= new_x < cols:
+                if not visited[new_y, new_x] and bitmap_bfs[new_y, new_x] != 1:
+                    visited[new_y, new_x] = True
+                    parent[(new_x, new_y)] = (x, y)
+                    queue.append((new_x, new_y))
+
+    # Get the actual path
+    if end not in parent:
+        print("BFS: Not actual path found\n")
+        return bitmap_bfs, []
+    
+    path = []
+    directions = []
+    position = end
+
+    while position != start:
+        path.append(position)
+
+        prev = parent[position]
+        path_to_x = position[0] - prev[0]
+        path_to_y = position[1] - prev[1]
+        directions.append((path_to_y, path_to_x))
+        
+        position = prev
+    
+    path.append(start)
+    path.reverse()
+    directions.reverse()
+
+    # Draw path on bitmap
+    for (x,y) in path:
+        bitmap_bfs[y,x] = 4
+
+    return bitmap_bfs, directions
